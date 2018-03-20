1f7925109f5e64901492acb07f5311e35b89e1c6
zz930316 <623381919@qq.com>
2018-03-20 18:22:02 +0800

    菜单栏，工具栏，状态栏，核心控件，浮动窗口的写法

--- 01/05_QMainWindow/mymainwindow.cpp
+++ 01/05_QMainWindow/mymainwindow.cpp
@@ -16,61 +16,80 @@ MyMainWindow::MyMainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     resize(500,400);
-   //菜单栏
-    QMenuBar *mBar = menuBar();//不是new，
+    //菜单栏
+    QMenuBar *mBar = menuBar();//不是new，menuBar()函数中自动new了
+
         //添加菜单
-    QMenu *pFile =mBar->addMenu("文件");
-            //添加菜单项,添加动作
-    QAction *pNew = pFile->addAction("新建");
+    QMenu *pFlie = mBar->addMenu("文件");
+    QMenu *pEdit = mBar->addMenu("编辑");
+
+        //添加菜单项，添加动作
+    QAction *pNew = pFlie->addAction("新建");
+    QAction *pAdd = pEdit->addAction("添加");
+    pFlie->addSeparator();//添加分割线
+    QAction *pOpen = pFlie->addAction("打开");
+
     connect(pNew,&QAction::triggered,
             [=]()
-            {
-                qDebug()<<"新建被按下";
-            });
+    {
+       qDebug()<<"新建被按下";
+    }
+                );
 
-    pFile->addSeparator();//添加分割线
+    //工具栏
+        //创建工具栏
+    QToolBar * toolBar = addToolBar("toolBar");
 
-    QAction *pOpen = pFile->addAction("打开");
-
-    //工具栏，菜单项的快捷方式
-    QToolBar *toolBar = addToolBar("toolBar");
-
-        //工具栏添加快捷键
+        //工具栏添加快键键
     toolBar->addAction(pNew);//直接把新建的指针写上去就可以
 
         //将按钮添加到工具栏，
     QPushButton *b = new QPushButton(this);
     b->setText("点击改名");
         //添加小控件
+        //工具栏上添加   工具栏对象->addWidget(添加一个按钮或者别的什么)
     toolBar->addWidget(b);
     connect(b,&QPushButton::clicked,
-            [=]()
+           [=]()
             {
-                b->setText("+=+");
-                qDebug()<<"改名被按下了";
-            });
+               b->setText("123");
+               qDebug()<<"改名被按下了";
+            }
+            );
+
 
     //状态栏
-    QStatusBar *sBar = statusBar();
+        //创建状态栏
+    QStatusBar *sBar= statusBar();
+
+        //设置标签，并取名
     QLabel *label = new QLabel(this);
-    label->setText("abcdefg");
-    sBar->addWidget(label);
+    QLabel *label1=new QLabel(this);
+    label->setText("打开成功");
+    label1->setText(" 123 ");
 
-        //addWidget是从左往右添加
-    sBar->addWidget(new QLabel("123",this));
+        //设置添加的方向位置；不设置的话就是在左上角显示了
+    sBar->addWidget(label);//从左下角开始往右添加
+    sBar->addPermanentWidget(label1);//从右下角向左添加
+        //通过构造函数
+    sBar->addPermanentWidget(new QLabel(" 构造函数传参 ",this));
 
-         //addPermanentWidget是从右往做左添加
-    sBar->addPermanentWidget(new QLabel("654",this));
-
-    //核心控件(空白区域是文本编辑区)
-    QTextEdit *textEdit = new QTextEdit();
-    setCentralWidget(textEdit);//设置主要控件
+    //核心控件
+        //创建一个文本编辑区
+    QTextEdit *textEdit = new QTextEdit(this);
+    setCentralWidget(textEdit);//set 设置 Central 中心 Widget部件
 
     //浮动窗口
-    QDockWidget *dock = new QDockWidget(this);
-    addDockWidget(Qt::RightDockWidgetArea,dock);
-    QTextEdit *tEdit = new QTextEdit();
-    dock->setWidget(tEdit);
+        //创建一个浮动窗口对象
+    QDockWidget *dockWidget = new QDockWidget(this);
+        /*参数一是浮动窗口的位置
+          参数二是浮动窗口对象*/
+    addDockWidget(Qt::RightDockWidgetArea,dockWidget);
+
+
+        // 给浮动窗口添加一个文本编辑区
+    QTextEdit *textEdit1 = new QTextEdit(this);
+    dockWidget->setWidget(textEdit1);
 
 }
 
